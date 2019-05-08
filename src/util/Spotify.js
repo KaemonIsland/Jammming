b68c537e351404e7fc2288f0fcbd21cf49e3f01e
KaemonIsland <k-manskrazy@hotmail.com>
2019-05-07 20:04:52 -0600

    Added Bootstrap, and Styled Components

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,5 +1,6 @@
 const clientId = '43585f07f1874d138fad92fc43e25663';
-const redirectUri = 'http://kaemon-jammming.surge.sh/';
+const redirectUri = 'http://localhost:3000/';
+//http://kaemon-jammming.surge.sh/
 
 let accessToken;
 
